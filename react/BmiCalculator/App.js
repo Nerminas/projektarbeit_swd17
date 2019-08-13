2057d7643dea586c21e1e1546e04948a43343d52
David Reindl <david.reindl@edu.fh-joanneum.at>
2019-08-13 22:19:00 +0200

    [REACT] styleless version of bmi calculation
    - litte input validation (isNaN)
    - bmi calculation

--- react/BmiCalculator/App.js
+++ react/BmiCalculator/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { View, StyleSheet, SafeAreaView, StatusBar, Platform } from 'react-native';
 import BmiCalculator from './screens/BmiCalculator';
 
-export default class App extends React.Component {
-  render() {
-    return (<BmiCalculator/>);
+export default class App extends React.Component{
+  render(){
+    console.log('Height on: ', Platform.OS, StatusBar.currentHeight);
+
+    return (
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.container}>
+          <BmiCalculator/>
+        </View>
+      </SafeAreaView>
+    );
   }
-};
+}
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+});
+
--- react/BmiCalculator/screens/BmiCalculator.js
+++ react/BmiCalculator/screens/BmiCalculator.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Image, StyleSheet, ScrollView, Text, View } from 'react-native';
-import { PricingCard, Header } from 'react-native-elements';
-import BottomNavBar from '../components/BottomNavBar';
 import { TextInput } from 'react-native-paper';
 
 export default class BmiCalculator extends React.Component{
@@ -10,11 +8,34 @@ export default class BmiCalculator extends React.Component{
     this.state = {
       height: 0,
       weight: 0,
-      bmi: 0,
+      errorWeight: false,
+      errorHeight: false,
     };
   }
 
+  calcBmi(height, weight){
+    return (weight / ((height / 100) * (height / 100))).toFixed(2);
+  }
+
+  validateInput(input){
+
+  }
+
   render(){
+    let bmi = '';
+    if (this.state.errorWeight || this.state.errorHeight){
+      bmi = 'Numeric error';
+    }else if (this.state.height != 0
+      && this.state.weight != 0){
+      let temp = this.calcBmi(this.state.height, this.state.weight);
+      console.log(this.state.weight);
+      console.log(this.state.height);
+
+      if (temp > 0 && temp < 100){
+        bmi = temp;
+      }
+    }
+
     return (
       <View style={styles.container}>
         <TextInput
@@ -22,10 +43,28 @@ export default class BmiCalculator extends React.Component{
           keyboardType='numeric'
           placeholder='weight'
           onChangeText={weight => {
-            this.state.setState({weight})
+            if (!isNaN(weight)){
+              this.setState({weight});
+              this.setState({errorWeight: false});
+            } else{
+              this.setState({errorWeight: true});
+            }
           }}
         />
-        <Text style={styles.output}>{this.state.bmi}</Text>
+        <TextInput
+          style={styles.input}
+          keyboardType='numeric'
+          placeholder='height'
+          onChangeText={height => {
+            if (!isNaN(height)){
+              this.setState({height});
+              this.setState({errorHeight: false});
+            } else{
+              this.setState({errorHeight: true});
+            }
+          }}
+        />
+        <Text style={styles.output}>{bmi}</Text>
       </View>
     );
   }
